8e1aadaccca19c982e07c3ad15cf2d0ef016f2a0
Eman Gurung <egurung1117@gmail.com>
2023-08-05 14:57:06 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+function findMatching(arr, name) {
+    const newArr = arr.filter((currentName) => {
+        return currentName.toUpperCase() === name.toUpperCase();
+    })
+
+    return newArr;
+}
+
+function fuzzyMatch(arr, letter) {
+    return arr.filter((currentName) => 
+        currentName.toUpperCase().indexOf(letter.toUpperCase()) === 0
+    );
+}
+
+
+
+function matchName(arr, name) {
+    const newArr = arr.filter((currentName) => {
+        return currentName.name === name;
+    })
+
+    return newArr;
+}
